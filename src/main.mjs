13e31321c44c1b077f547f546c73eb54c71d87b1
fredydlemus <fredyflemus@gmail.com>
2022-05-05 22:37:18 -0600

    before add axios

--- src/main.mjs
+++ src/main.mjs
@@ -1,3 +1,4 @@
+
 async function getTrendingMoviesPreview(){
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key='+API_KEY);
     const data = await res.json();
@@ -20,4 +21,27 @@ async function getTrendingMoviesPreview(){
 
 }
 
+async function getCategoriesPreview(){
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key='+API_KEY);
+    const data = await res.json();
+    const categories = data.genres;
+    categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id'+category.id);
+        categoryTitle.innerHTML = category.name;
+
+        categoryContainer.appendChild(categoryTitle);
+        previewCategoriesContainer.appendChild(categoryContainer);
+    });
+
+
+}
+
 getTrendingMoviesPreview();
+getCategoriesPreview();
